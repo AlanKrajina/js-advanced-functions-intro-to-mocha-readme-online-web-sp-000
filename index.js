44f4523cd71192bb13badccd3253519ecfd6fe65
Alan Krajina <alankrajina88@gmail.com>
2019-09-24 14:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(friend) {
+  return `Hey ${friend}!`
+}
+
 function favoriteIceCream(flavor) {
-  debugger;
+  return `I love ${flavor}`
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+
+function roundDown(number) {
+  return Math.round(number)
 }
